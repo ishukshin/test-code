ca2736df1ac9ceebe58a69c44328864740db2020
Shukshin Ivan <ioann@krutika.ru>
2015-07-09 13:40:03 +0400

    fix

--- sql2.php
+++ sql2.php
@@ -7,7 +7,7 @@ $stmt = $pdo->prepare('SELECT s.name, s.surname FROM student s '.
         'INNER JOIN ( '.
         ' SELECT student_id, MAX( `datetime` ) as `datetime` ' . 
         ' FROM  `student_status` GROUP BY student_id) st2 ' . 
-        ' ON st2.datetime = st.datetime WHERE s.gender = :gender AND st.status = :status');
+        ' ON st2.datetime = st.datetime AND st.student_id = st2.student_id WHERE s.gender = :gender AND st.status = :status');
 
 $stmt->execute([
     'gender' => 'unknown',
